402f521c3e2e641529550eb8faa09a575f131a6b
Felipe Cabrera <me@felieppe.com>
2024-09-18 09:23:14 -0300

    feat: add TA13 page

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,7 @@ function Home() {
                     <li><Link href="/ta10">TA 10</Link></li>
                     <li><Link href="/ta11">TA 11</Link></li>
                     <li><Link href="/ta12">TA 12</Link></li>
+                    <li><Link href="/ta13">TA 13</Link></li>
                 </ol>
             </div>
         </div>
---
+++ pages/ta13.jsx
@@ -0,0 +1,39 @@
+import styles from '../styles/TA13.module.css'
+
+import { useState, useContext } from 'react';
+import { UserContext } from './_app';
+import UserDisplay from '../components/userDisplay';
+
+function TA13() {
+    const { username, setUsername } = useContext(UserContext);
+    const [display, setDisplay] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let username = e.target[0].value;
+        setUsername(username);
+        setDisplay(true)
+    }
+
+    const handleChange = (e) => {
+        setUsername(e.target.value);
+        setDisplay(true);
+    }
+
+    return (
+        <div className="container">
+            <div className={styles.ta13}>
+                <h1 className={styles.ta13__title}>TA 13</h1>
+                
+                <form className={styles.ta13__form}>
+                    <input type="text" placeholder='@username' onChange={handleChange} required/>
+                </form>
+
+                {display && username != '' ? <UserDisplay /> : null}
+            </div>
+        </div>
+    )
+}
+
+export default TA13;
